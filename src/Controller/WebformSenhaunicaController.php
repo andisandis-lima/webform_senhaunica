7e13c58e7331004e95d721704e3323c42721a2ae
andisandis-lima <anderson.ls1998@gmail.com>
2026-06-26 17:19:16 -0300

    Controller Anderson.php apagado, não era mais necessário. Adionado tipagem e Injeção de Dependencia (RequestStack e MessegerInterface) no controller WebformSenhaunicaController.php

--- src/Controller/WebformSenhaunicaController.php
+++ src/Controller/WebformSenhaunicaController.php
@@ -4,38 +4,61 @@ declare(strict_types=1);
 
 namespace Drupal\webform_senhaunica\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Uspdev\Senhaunica\Senhaunica;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-/**
- * Returns responses for Webform Senha Única routes.
- */
-final class WebformSenhaunicaController extends ControllerBase {
-  public function __invoke(): array|Response {
-    
+final class WebformSenhaunicaController implements ContainerInjectionInterface {
+  public function __construct(
+    private RequestStack $requestStack,
+    private MessengerInterface $messenger
+  ) {
+  }
+
+  public static function create(ContainerInterface $container): static {
+  return new static(
+    $container->get('request_stack'),
+    $container->get('messenger')
+  );
+}
+  public function __invoke(): RedirectResponse {
+
     Senhaunica::login();
+
+    /**
+     * @var array{
+     *   loginUsuario: string,
+     *   nomeUsuario: string,
+     *   emailPrincipalUsuario?: string,
+     *   emailAlternativoUsuario?: string,
+     *   emailUspUsuario?: string,
+     *   wsuserid: string,
+     *   webform_id: string
+     * }  $user */
     $user = Senhaunica::getUserDetail();
 
-    $session = \Drupal::request()->getSession();
+    $session = $this->requestStack->getSession();
 
     // Armazenar dados do usuário na sessão para usar na validação
     $session->set('senhaunica_numero_usp', $user['loginUsuario']);
     $session->set('senhaunica_nome_usuario', $user['nomeUsuario']);
-    $session->set('senhaunica_email', ($user['emailPrincipalUsuario'] ?? '')
-      ?: ($user['emailAlternativoUsuario'] ?? '')
-      ?: ($user['emailUspUsuario'] ?? ''));
+    $session->set('senhaunica_email', $user['emailPrincipalUsuario'] ?? $user['emailAlternativoUsuario']
+      ?? $user['emailUspUsuario'] ?? '');
     $session->set('senhaunica_hash', $user['wsuserid']);
 
     $webform_id = $session->get('senhaunica_webform_id');
 
-    \Drupal::messenger()->addStatus(
-      t('Login realizado com sucesso. Você pode responder o formulário.')
-    );
+    $this->messenger->addStatus('Login realizado com sucesso.');
+
+    if (!is_string($webform_id) || $webform_id === '') {
+      throw new \RuntimeException('Webform ID inválido na sessão');
+    }
 
     // Redirecionar para o webform para responder
-    return new RedirectResponse("/webform/$webform_id");
+    return new RedirectResponse('/webform/' . $webform_id);
   }
 }
 
